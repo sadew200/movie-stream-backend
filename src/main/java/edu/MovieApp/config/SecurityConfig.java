fe4a0a1cb65069c7f9b0e7e3a6b8d0ebbe729241
sadew bathiya <158308526+sadew200@users.noreply.github.com>
2025-04-23 23:56:03 +0530

    Update SecurityConfig.java

--- src/main/java/edu/MovieApp/config/SecurityConfig.java
+++ src/main/java/edu/MovieApp/config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://creative-florentine-6d614b.netlify.app"));
+        config.setAllowedOrigins(List.of("https://courageous-moxie-16865b.netlify.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
