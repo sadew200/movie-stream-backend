3af0e073a9e7709cd5924b570d5ca1c5bb40aa46
sadew bathiya <158308526+sadew200@users.noreply.github.com>
2025-05-07 16:46:31 +0530

    Update JWTService.java

--- src/main/java/edu/MovieApp/Service/JWTService.java
+++ src/main/java/edu/MovieApp/Service/JWTService.java
@@ -40,7 +40,7 @@ public class JWTService {
                 .add(claims)
                 .subject(email)
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + 60 * 60 * 30))
+                .expiration(new Date(System.currentTimeMillis() + 5 * 60 * 60 * 1000)))
                 .and()
                 .signWith(getKey())
                 .compact();
